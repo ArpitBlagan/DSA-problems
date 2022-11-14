a41d8a429899b6df0acff7813060f74085329211
arpit <blaganarpit@gmail.com>
2022-11-14 19:09:02 +0530

    cool

--- RoboFusion.cpp
+++ RoboFusion.cpp
@@ -7,12 +7,12 @@ using namespace std;
 #define ll long long
 const ll INF=1e18;
 const int mod=1e5+7;
-int dfs(int i,int j,vector<int>&arr,vector<vector<int>>&dp){
+int dfs(int i,int j,vector<pair<int,int>>&arr,vector<vector<int>>&dp){
 	if(i>=j){return 0;}
 	if(dp[i][j]!=-1){return dp[i][j];}
 	int ans=INT_MAX;
-	for(int ii=i;ii<=j-1;ii++){
-		int val=dfs(i,ii,arr,dp)+dfs(ii+1,j,arr,dp)+arr[i-1]*arr[ii]*arr[j];
+	for(int ii=i;ii<j-1;ii++){
+		int val=dfs(i,ii,arr,dp)+dfs(ii+1,j,arr,dp)+arr[i].first*arr[ii].second*arr[j].second;
 		ans=min(ans,val);
 	}
 	return dp[i][j]=ans;
@@ -25,6 +25,6 @@ int main(){
 	}vector<int>vall;vall.push_back(arr[0].first);vall.push_back(arr[0].second);
 	for(int i=1;i<n;i++){vall.push_back(arr[i].second);}
 	vector<vector<int>>dp(n+2,vector<int>(n+2,-1));
-	cout<<dfs(1,n,vall,dp);
+	cout<<dfs(0,n-1,arr,dp);
 	return 0;
 }
